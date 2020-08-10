07f02a9842bcfc3acb8fd0875cfae8533ceb0a1e
McCall <1059511459@qq.com>
2020-08-10 10:32:01 +0800

    deploy

---
+++ precache-manifest.8eff0ce7c6c2248e5982da82540fc1ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d81be9ef7dbbf829ea",
+    "url": "/whale-wallet-website/css/app.99295777.css"
+  },
+  {
+    "revision": "ecde24c581a79a64c6b4a86bf7b51623",
+    "url": "/whale-wallet-website/index.html"
+  },
+  {
+    "revision": "07d81be9ef7dbbf829ea",
+    "url": "/whale-wallet-website/js/app.dff768d7.js"
+  },
+  {
+    "revision": "91cab80eb96913b00ce8",
+    "url": "/whale-wallet-website/js/chunk-vendors.2c97417b.js"
+  },
+  {
+    "revision": "dc8525057bee1eea0e4faa08ff18d425",
+    "url": "/whale-wallet-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/whale-wallet-website/robots.txt"
+  }
+]);
